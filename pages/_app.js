23cb1c359d6b11c2ddfa5148f8d2bf6fa5b4c1e4
Karan Shetty <shettykaran2110@gmail.com>
2021-10-26 19:45:54 +0530

    Material UI setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import { CacheProvider } from '@emotion/react'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '@styles/globals.css'
+import theme from '@styles/theme'
+import createEmotionCache from '@styles/createEmotionCache'
+
+const clientSideEmotionCache = createEmotionCache()
+
+const MyApp = ({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}) => {
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>Forum</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
+import Header from '@components/layout/header'
+
 const HomePage = () => {
-  return <div>App</div>
+  return (
+    <div>
+      <Header />
+    </div>
+  )
 }
 
 export default HomePage
