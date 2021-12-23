c51cfebcce4b311e6e9bcdf90146469845f7efdf
Karan Shetty <shettykaran2110@gmail.com>
2021-12-23 22:27:10 +0530

    Fetch questions by tag

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 import { Box, Typography } from '@mui/material'
 
 import Layout from '@components/layout'
@@ -6,13 +8,32 @@ import Main from '@components/layout/main'
 import api from '@utils/api'
 
 const HomePage = ({ questions }) => {
+  const [questionsList, setQuestionsList] = useState(questions)
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const fetchQuestionByTag = async () => {
+      const { data } = await api.get(`/questions`, {
+        params: {
+          tag: router.query.tag,
+        },
+      })
+      setQuestionsList(data.data)
+    }
+
+    if (router.query.tag) {
+      fetchQuestionByTag()
+    }
+  }, [router.query.tag])
+
   return (
     <>
       <Head>
         <title>Forum</title>
       </Head>
       <Layout>
-        <Main questions={questions} />
+        <Main questions={questionsList} />
         <Box sx={{ flexBasis: '35%', paddingTop: '3rem' }}>
           <Typography variant="body1">Popular Tags</Typography>
         </Box>
