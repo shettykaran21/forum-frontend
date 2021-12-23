d906db1657dc34c67d38db51f7a5556bb1b996fb
Karan Shetty <shettykaran2110@gmail.com>
2021-12-24 02:32:27 +0530

    Use MUI form inputs

--- components/form/form-input/index.js
+++ components/form/form-input/index.js
@@ -1,57 +1,17 @@
-/** @jsxImportSource @emotion/react */
+import { TextField } from '@mui/material'
 
-import { css } from '@emotion/react'
-import { Box, Typography } from '@mui/material'
-import { lighten } from '@mui/material/styles'
-
-const FormInput = ({
-  label,
-  name,
-  inputInfo,
-  hasError = false,
-  errorMsg,
-  ...props
-}) => {
-  const styles = {
-    container: css`
-      display: flex;
-      flex-direction: column;
-      position: relative;
-      padding-top: 1.5rem;
-    `,
-    label: css`
-      display: block;
-      color: #555;
-    `,
-    input: css`
-      border: 0;
-      z-index: 1;
-      background-color: transparent;
-      border-bottom: 2px solid #eee;
-      font: inherit;
-      font-size: 1rem;
-      padding: 0.25rem 0;
-
-      &:focus {
-        outline: 0;
-        border-bottom-color: #6658d3;
-      }
-    `,
-    error: css`
-      color: ${lighten('#ff0000', 0.5)};
-      font-size: 0.875rem;
-      margin-top: 0.5rem;
-    `,
-  }
-
+const FormInput = ({ label, name, hasError = false, errorMsg, ...props }) => {
   return (
-    <Box sx={styles.container}>
-      <label htmlFor={name} css={styles.label}>
-        {label}
-      </label>
-      <input id={name} css={styles.input} {...props} />
-      {hasError && <Typography sx={styles.error}>{errorMsg}</Typography>}
-    </Box>
+    <TextField
+      id={name}
+      label={label}
+      variant="standard"
+      fullWidth
+      error={hasError}
+      helperText={hasError && errorMsg}
+      margin="normal"
+      {...props}
+    />
   )
 }
 
--- components/form/form-password-input/index.js
+++ components/form/form-password-input/index.js
@@ -2,55 +2,19 @@
 
 import { useState } from 'react'
 import { css } from '@emotion/react'
-import { Box, Typography } from '@mui/material'
-import { lighten } from '@mui/material/styles'
+import { IconButton, InputAdornment, TextField } from '@mui/material'
 import { FaEye, FaEyeSlash } from 'react-icons/fa'
 
 const FormPasswordInput = ({
   label,
   name,
-  inputInfo,
   hasError = false,
   errorMsg,
   ...props
 }) => {
   const styles = {
-    container: css`
-      display: flex;
-      flex-direction: column;
-      position: relative;
-      padding-top: 1.5rem;
-    `,
-    label: css`
-      display: block;
-      color: #555;
-    `,
-    input: css`
-      border: 0;
-      z-index: 1;
-      background-color: transparent;
-      border-bottom: 2px solid #eee;
-      font: inherit;
-      font-size: 1rem;
-      padding: 0.25rem 0;
-      flex: 1;
-
-      &:focus {
-        outline: 0;
-        border-bottom-color: #6658d3;
-      }
-    `,
-    error: css`
-      color: ${lighten('#ff0000', 0.5)};
-      font-size: 0.875rem;
-      margin-top: 0.5rem;
-    `,
     icon: css`
-      color: #333;
-      cursor: pointer;
-      position: absolute;
-      right: 0;
-      z-index: 1;
+      font-size: 1.25rem;
     `,
   }
 
@@ -61,31 +25,30 @@ const FormPasswordInput = ({
   }
 
   return (
-    <Box sx={styles.container}>
-      <label htmlFor={name} css={styles.label}>
-        {label}
-      </label>
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <input
-          id={name}
-          css={styles.input}
-          {...props}
-          type={isVisible ? 'text' : 'password'}
-        />
-        {!isVisible ? (
-          <FaEye
-            css={styles.icon}
-            onClick={() => toggleVisibility(isVisible)}
-          />
-        ) : (
-          <FaEyeSlash
-            css={styles.icon}
-            onClick={() => toggleVisibility(isVisible)}
-          />
-        )}
-      </Box>
-      {hasError && <Typography sx={styles.error}>{errorMsg}</Typography>}
-    </Box>
+    <TextField
+      id={name}
+      label={label}
+      variant="standard"
+      fullWidth
+      type={isVisible ? 'text' : 'password'}
+      error={hasError}
+      helperText={hasError && errorMsg}
+      margin="normal"
+      {...props}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <IconButton onClick={() => toggleVisibility(isVisible)}>
+              {isVisible ? (
+                <FaEye css={styles.icon} />
+              ) : (
+                <FaEyeSlash css={styles.icon} />
+              )}
+            </IconButton>
+          </InputAdornment>
+        ),
+      }}
+    />
   )
 }
 
