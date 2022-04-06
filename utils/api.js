0204764cc5479c7f0bc6749b634d1722e0b57af7
Karan Shetty <shettykaran2110@gmail.com>
2022-04-06 17:24:12 +0530

    Added server website name to env file

--- utils/api.js
+++ utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const baseURL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8080'
-    : `https://${process.env.WEBSITE_NAME}`
+    : process.env.WEBSITE_NAME
 
 const api = axios.create({
   baseURL,
